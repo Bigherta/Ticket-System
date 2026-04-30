c07bf6d0fe23fe57247e58d138050776eddaf475
SEED <hhs_sjtu@sjtu.edu.cn>
2026-04-30 13:01:59 +0800

    update : adjust the buffer pool size to 5000 and order to 48

--- BPlusTree/BPT.hpp
+++ BPlusTree/BPT.hpp
@@ -5,7 +5,7 @@
 #include "BufferPoolManager.hpp"
 #include "utility.hpp"
 #include "vector.hpp"
-constexpr int order = 56;
+constexpr int order = 48;
 template<class T>
 int BinarySearch(const T arr[], int size, const T &key) // upper_bound
 {
@@ -453,7 +453,7 @@ public:
         BPTree.get_info(root_pos, 1);
         BPTree.get_info(tree_size, 2);
 
-        bufferPool = new BufferPoolManager<Node<order>>(2500, BPTree, root_pos);
+        bufferPool = new BufferPoolManager<Node<order>>(5000, BPTree, root_pos);
     }
     ~BPT()
     {
